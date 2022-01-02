ec6ae373f402ea954035f866786ef87b15ea8cae
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-03 04:48:46 +0530

    added ReviewsTableRow.js

---
+++ client/src/components/DashBoard/MainInfo/ReviewsTableRow.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Info from "./Info";
+import Review from "./Review";
+import TableData from "./Table/TableData";
+import TableRow from "./Table/TableRow";
+
+function ReviewsTableRow({
+  tableData,
+  setCommentReply,
+  setActionWithId,
+  type,
+  Id
+}) {
+  function setAction(type) {
+    setActionWithId({ type, Id });
+  }
+  function setReply(comment) {
+    setCommentReply({ comment, id: tableData.id });
+  }
+  return (
+    <TableRow>
+      <TableData type={type}>
+        <Review data={tableData} setReply={setReply}></Review>
+      </TableData>
+      <TableData>
+        <div className="flex space-x-2 duration-300 rounded-md ">
+          <div className="w-14 h-14 rounded-md overflow-hidden">
+            <img
+              src={tableData.product.img}
+              className="w-full h-full object-cover"
+              alt=""
+            />
+          </div>
+          <div className="flex flex-1 items-center justify-between">
+            <div className="font-normal">
+              <div className="font-normal  capitalize">
+                {tableData.product.name}
+              </div>
+              <div className="text-sm text-[#4e4e4e] capitalize">
+                {tableData.product.category}
+              </div>
+            </div>
+          </div>
+        </div>
+      </TableData>
+      <TableData className="px-1.5 py-2.5">
+        <Info clickedAction={setAction} type={type}></Info>
+      </TableData>
+    </TableRow>
+  );
+}
+
+export default ReviewsTableRow;
