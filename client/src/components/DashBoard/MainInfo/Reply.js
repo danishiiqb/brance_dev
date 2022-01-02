543097b0633be041f18290262a6d052b28865a49
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-03 04:47:36 +0530

    added Reply.js

---
+++ client/src/components/DashBoard/MainInfo/Reply.js
@@ -0,0 +1,42 @@
+import React, { useRef } from "react";
+
+function Reply({ closeReply, setReply }) {
+  const inputRef = useRef("");
+  return (
+    <div className="flex space-x-2 items-center mt-2">
+      <div>
+        <img
+          src="/img/dashboard/profile.jpeg"
+          className="w-6 h-6 object-cover rounded-full"
+          alt=""
+        />
+      </div>
+      <input
+        ref={inputRef}
+        className="flex-1 duration-300 p-0.5 group-hover:bg-[#f5f5f5] border-b-[1px] border-gray-400 text-sm  focus:outline-none bg"
+        type="text"
+        placeholder="Add a reply"
+      />
+      <div className="text-xs space-x-2">
+        <button
+          onClick={() => {
+            closeReply(false);
+          }}
+        >
+          Cancel
+        </button>
+        <button
+          onClick={() => {
+            setReply(inputRef.current.value);
+            closeReply(false);
+          }}
+          className="p-1 text-white rounded-sm bg-[#FF385C]"
+        >
+          Reply
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Reply;
