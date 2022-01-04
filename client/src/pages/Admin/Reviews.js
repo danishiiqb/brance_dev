df354077347fe0d685479c7440a9a1ac1da8b6b4
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-05 04:31:25 +0530

    added likes dislike reviews func

--- client/src/pages/Admin/Reviews.js
+++ client/src/pages/Admin/Reviews.js
@@ -15,7 +15,11 @@ function Reviews() {
         message:
           "Very bad experience. Gave money on 17th till date could not use the laptop as we have received lower configuration. Will not buy hereafter from flipkart.will say friends and relatives also. Will file a civil suit on Monday.",
         time: "9h ago",
-        rating: "4"
+        rating: "4",
+        engageMent: {
+          likes: [{ user: "John" }],
+          dislikes: [{ user: "Jio" }]
+        }
       },
       product: {
         img: "/img/product.png",
@@ -31,7 +35,11 @@ function Reviews() {
         message:
           "Very bad experience. Gave money on 17th till date could not use the laptop as we have received lower configuration. Will not buy hereafter from flipkart.will say friends and relatives also. Will file a civil suit on Monday.",
         time: "9h ago",
-        rating: "4"
+        rating: "4",
+        engageMent: {
+          likes: [],
+          dislikes: []
+        }
       },
       product: {
         img: "/img/product.png",
@@ -61,9 +69,8 @@ function Reviews() {
       return newReview;
     });
   }
+
   function setActionWithId(obj) {
-    if (obj.type === "Edit") {
-    }
     if (obj.type === "Delete") {
       deleteOwnerReview(obj.id);
       return;
@@ -89,6 +96,55 @@ function Reviews() {
       return newReviews;
     });
   }
+  function checkExistingLikes(user, likesArray) {
+    let alreadyLiked = likesArray.findIndex((likes) => {
+      return likes.name === user.name;
+    });
+    if (alreadyLiked !== -1) {
+      return likesArray.filter((_, idx) => {
+        return idx !== alreadyLiked;
+      });
+    }
+    return [...likesArray, user];
+  }
+  function incDecEngagement(obj) {
+    setReviews((prev) => {
+      let newReviews = prev.map((review) => {
+        if (review.id === obj.id) {
+          return obj.engageInfo.type === "Like"
+            ? {
+                ...review,
+                product: { ...review.product },
+                comments: {
+                  ...review.comments,
+                  engageMent: {
+                    ...review.comments.engageMent,
+                    likes: checkExistingLikes(
+                      obj.engageInfo.user,
+                      review.comments.engageMent.likes
+                    )
+                  }
+                }
+              }
+            : {
+                ...review,
+                product: { ...review.product },
+                comments: {
+                  ...review.comments,
+                  dislikes: review.comments.dislikes + 1
+                }
+              };
+        }
+        return {
+          ...review,
+          product: { ...review.product },
+          comments: { ...review.comments }
+        };
+      });
+      return newReviews;
+    });
+  }
+
   return (
     <div className="h-screen">
       <div className="bg-white shadow-sm_dark rounded-md mt-6 p-small">
@@ -108,6 +164,7 @@ function Reviews() {
                 key={idx}
                 setCommentReply={setCommentReply}
                 tableData={tableData}
+                incDecEngagement={incDecEngagement}
               ></ReviewsTableRow>
             );
           })}
