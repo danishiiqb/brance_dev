63d68b753d2544f42b2b4068159df455bc2589e3
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-09 04:27:45 +0530

    added textArea.js

---
+++ client/src/components/DashBoard/MainInfo/NewProductAdd/TextArea.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function TextArea() {
+  return (
+    <div className="mt-4">
+      <label className="font-medium text-small" htmlFor="description">
+        Description
+      </label>
+      <textarea className="resize-none block mt-2 w-full  h-heightImg border-gray-300 border-[.5px] focus:border-[#FF385C]  focus:outline-none transition-all duration-200 text-small rounded-md py-2 px-2"></textarea>
+    </div>
+  );
+}
+
+export default TextArea;
