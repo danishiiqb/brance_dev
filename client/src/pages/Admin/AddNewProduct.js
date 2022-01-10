bb3174703fd6e7fa2d7530d3bbd7490cbb3c62cc
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-10 21:13:56 +0530

    fixed bugs

--- client/src/pages/Admin/AddNewProduct.js
+++ client/src/pages/Admin/AddNewProduct.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import ImageUploadForm from "../../components/DashBoard/MainInfo/NewProductAdd/ImageUploadForm";
 import NewProductForm from "../../components/DashBoard/MainInfo/NewProductAdd/NewProductForm";
 import SubmitButtons from "../../components/DashBoard/MainInfo/NewProductAdd/SubmitButtons";
 import TextArea from "../../components/DashBoard/MainInfo/NewProductAdd/TextArea";
 
 function AddNewProduct() {
+  const [formData, setFormData] = useState({ images: [] });
+  const collectValues = useCallback((val, type) => {
+    if (type === "IMG_UPLOAD") {
+      setFormData((prev) => {
+        return { ...prev, images: [...prev.images, val] };
+      });
+      return;
+    }
+    setFormData((prev) => {
+      return { ...prev, [type]: val };
+    });
+  }, []);
   return (
     <div className="h-panel">
       <div className="bg-white relative  space-x-6 h-full flex shadow-sm_dark rounded-md mt-6 p-small">
-        <NewProductForm></NewProductForm>
+        <NewProductForm collectValues={collectValues}></NewProductForm>
         <div>
-          <ImageUploadForm></ImageUploadForm>
-          <TextArea></TextArea>
+          <ImageUploadForm collectValues={collectValues}></ImageUploadForm>
+          <TextArea collectValues={collectValues}></TextArea>
         </div>
-        <SubmitButtons></SubmitButtons>
+        <SubmitButtons formData={formData}></SubmitButtons>
       </div>
     </div>
   );
