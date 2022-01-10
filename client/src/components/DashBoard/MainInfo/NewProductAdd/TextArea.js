1ddeea810e181c3317419cc5e91fb45442c7bc90
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-10 21:15:21 +0530

    added the functionality to get value

--- client/src/components/DashBoard/MainInfo/NewProductAdd/TextArea.js
+++ client/src/components/DashBoard/MainInfo/NewProductAdd/TextArea.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function TextArea() {
+function TextArea({ collectValues }) {
   return (
     <div className="mt-4">
       <label className="font-medium text-small" htmlFor="description">
         Description
       </label>
-      <textarea className="resize-none block mt-2 w-full  h-heightImg border-gray-300 border-[.5px] focus:border-[#FF385C]  focus:outline-none transition-all duration-200 text-small rounded-md py-2 px-2"></textarea>
+      <textarea
+        onChange={(e) => {
+          collectValues(e.target.value, "desc");
+        }}
+        className="resize-none block mt-2 w-full  h-heightImg border-gray-300 border-[.5px] focus:border-[#FF385C]  focus:outline-none transition-all duration-200 text-small rounded-md py-2 px-2"
+      ></textarea>
     </div>
   );
 }
