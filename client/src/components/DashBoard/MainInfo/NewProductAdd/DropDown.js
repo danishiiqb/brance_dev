c6d3c08539d275192cafb9612807015794193e18
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-10 21:16:16 +0530

    made changes to getSelected dropDown value

--- client/src/components/DashBoard/MainInfo/NewProductAdd/DropDown.js
+++ client/src/components/DashBoard/MainInfo/NewProductAdd/DropDown.js
@@ -1,47 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 
-function DropDown({ dropdownList, notifyParentForDetailDropDown = null }) {
+function DropDown({ dropdownList, getAllValues, title }) {
   const [dropdown, expandDropDown] = useState(false);
   const [selected, setSelected] = useState(dropdownList[0]);
 
+  useEffect(() => {
+    getAllValues(selected, title);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <ul className={`relative   mt-2  text-small  font-regular`}>
-      <li
-        onClick={() => {
-          expandDropDown((prev) => !prev);
-        }}
-        className={`flex ${
-          dropdown ? "shadow-sm rounded-t-md border-b-0" : "rounded-md "
-        }  cursor-pointer border-gray-300 border-[.5px]border-gray-300 border-[.5px] p-2 justify-between  items-center capitalize `}
-      >
-        <div>{selected}</div>
-        <IoMdArrowDropdown className="w-4 h-4"></IoMdArrowDropdown>
-      </li>
-      {dropdown && (
-        <ul className="absolute z-50 left-0 top-full w-full h-full  ">
-          {dropdownList.map((elem, idx) => {
-            if (selected === elem) {
-              return null;
-            }
-            return (
-              <li
-                onClick={() => {
-                  setSelected(elem);
-                  notifyParentForDetailDropDown &&
-                    notifyParentForDetailDropDown(elem);
-                  expandDropDown(false);
-                }}
-                key={idx}
-                className="p-2 bg-white relative border-gray-300 border-r-[.5px] border-l-[.5px] hover:bg-gray-50 capitalize last:border-b-[.5px] last:rounded-b-md  cursor-pointer "
-              >
-                {elem}
-              </li>
-            );
-          })}
-        </ul>
+    <>
+      {title !== "currency" && (
+        <div className="font-medium text-small capitalize">{title}</div>
       )}
-    </ul>
+      <ul className={`relative   mt-2  text-small  font-regular`}>
+        <li
+          onClick={() => {
+            expandDropDown((prev) => !prev);
+          }}
+          className={`flex ${
+            dropdown ? "shadow-sm rounded-t-md border-b-0" : "rounded-md "
+          }  cursor-pointer border-gray-300 border-[.5px]border-gray-300 border-[.5px] p-2 justify-between  items-center capitalize `}
+        >
+          <div>{selected}</div>
+          <IoMdArrowDropdown className="w-4 h-4"></IoMdArrowDropdown>
+        </li>
+        {dropdown && (
+          <ul className="absolute z-50 left-0 top-full w-full h-full  ">
+            {dropdownList.map((elem, idx) => {
+              if (selected === elem) {
+                return null;
+              }
+              return (
+                <li
+                  onClick={() => {
+                    setSelected(elem);
+                    expandDropDown(false);
+                    getAllValues(elem, title);
+                  }}
+                  key={idx}
+                  className="p-2 bg-white relative border-gray-300 border-r-[.5px] border-l-[.5px] hover:bg-gray-50 capitalize last:border-b-[.5px] last:rounded-b-md  cursor-pointer "
+                >
+                  {elem}
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </ul>
+    </>
   );
 }
 
