96e82f376828d4e8e2bbe86b677fbb13094a8aa2
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-17 03:28:33 +0530

    added validation for form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,22 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./DashBoard/MainInfo/NewProductAdd/Input";
 
 function Form() {
+  const [formData, setformData] = useState({ email: "", password: "" });
+  const [err, setErr] = useState({ type: "", mssg: "" });
+
+  const changeFormData = (val, type) => {
+    setErr({ type: "", mssg: "" });
+    setformData((prev) => {
+      return { ...prev, [type]: val };
+    });
+  };
+
+  function submitHandler(e) {
+    e.preventDefault();
+    if (!formData.password || !formData.email) {
+      setErr({ type: "Not Entered", mssg: "Enter all the fields" });
+      return;
+    }
+  }
   return (
-    <form className="">
+    <form onSubmit={submitHandler}>
+      {err.type === "Not Entered" && (
+        <div className="text-xs text-[#FF385C] mb-2">{err.mssg}</div>
+      )}
       <div>
         <label className="font-medium text-small" htmlFor="email">
           Email Address
         </label>
-        <Input placeholder="Enter Email" type="email" id="email"></Input>
+        <Input
+          getAllValues={changeFormData}
+          placeholder="Enter Email"
+          value={formData.email}
+          type="email"
+          id="email"
+        ></Input>
       </div>
-      <div className="mt-4">
+      <div className="mt-3">
         <label className="font-medium text-small" htmlFor="password">
           Password
         </label>
         <Input
+          getAllValues={changeFormData}
           placeholder="Enter Password"
           type="password"
+          value={formData.password}
           id="password"
         ></Input>
       </div>
