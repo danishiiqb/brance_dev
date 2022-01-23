ac21d964d7fda8a4f0c548d49126f64ad3d690b4
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-24 02:29:22 +0530

    added clear form functionality

--- client/src/pages/Admin/AddNewProduct.js
+++ client/src/pages/Admin/AddNewProduct.js
@@ -1,15 +1,28 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import ImageUploadForm from "../../components/DashBoard/MainInfo/NewProductAdd/ImageUploadForm";
 import NewProductForm from "../../components/DashBoard/MainInfo/NewProductAdd/NewProductForm";
 import SubmitButtons from "../../components/DashBoard/MainInfo/NewProductAdd/SubmitButtons";
 import TextArea from "../../components/DashBoard/MainInfo/NewProductAdd/TextArea";
 
 function AddNewProduct() {
-  const [formData, setFormData] = useState({ images: [] });
+  const [formData, setFormData] = useState({ images: [], desc: "" });
   const [mssg, setMssg] = useState("");
+  const [clear, setClear] = useState(false);
+  function clearForm() {
+    setClear(true);
+  }
+
+  useEffect(() => {
+    if (clear) {
+      setFormData((prev) => {
+        return { ...prev, images: [], desc: "" };
+      });
+    }
+  }, [clear]);
 
   const collectValues = useCallback((val, type) => {
     setMssg("");
+    setClear(false);
     if (type === "IMG_UPLOAD") {
       setFormData((prev) => {
         return {
@@ -26,20 +39,35 @@ function AddNewProduct() {
       return { ...prev, [type]: val };
     });
   }, []);
+
   return (
     <div className="h-panel">
       <div className="bg-white relative  space-x-6 h-full flex shadow-sm_dark rounded-md mt-6 p-small ">
-        <NewProductForm collectValues={collectValues}></NewProductForm>
+        <NewProductForm
+          collectValues={collectValues}
+          clear={clear}
+        ></NewProductForm>
         <div>
-          <ImageUploadForm collectValues={collectValues}></ImageUploadForm>
-          <TextArea collectValues={collectValues}></TextArea>
+          <ImageUploadForm
+            clear={clear}
+            setMssg={setMssg}
+            collectValues={collectValues}
+          ></ImageUploadForm>
+          <TextArea
+            formData={formData}
+            collectValues={collectValues}
+          ></TextArea>
           {mssg && (
             <div className="absolute mt-2 text-xs text-[#FF385C] mb-2 z-50">
               {mssg}
             </div>
           )}
         </div>
-        <SubmitButtons formData={formData} setMssg={setMssg}></SubmitButtons>
+        <SubmitButtons
+          formData={formData}
+          clearForm={clearForm}
+          setMssg={setMssg}
+        ></SubmitButtons>
       </div>
     </div>
   );
