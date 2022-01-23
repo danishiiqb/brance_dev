7b49919d7a7b3165ca0204a3102e64e86bcb7802
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-24 04:26:00 +0530

    fixed clear issue

--- client/src/components/DashBoard/MainInfo/NewProductAdd/TextArea.js
+++ client/src/components/DashBoard/MainInfo/NewProductAdd/TextArea.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function TextArea({ collectValues }) {
+function TextArea({ collectValues, formData }) {
   return (
     <div className="mt-4">
       <label className="font-medium text-small" htmlFor="description">
@@ -10,6 +10,7 @@ function TextArea({ collectValues }) {
         onChange={(e) => {
           collectValues(e.target.value, "desc");
         }}
+        value={formData.desc}
         className="resize-none block mt-2 w-full  h-heightImg border-gray-300 border-[.5px] focus:border-[#FF385C]  focus:outline-none transition-all duration-200 text-small rounded-md py-2 px-2"
       ></textarea>
     </div>
