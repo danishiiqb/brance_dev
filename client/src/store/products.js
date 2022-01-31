5de4fe6a565c3eaabd30a1ae46d516bc4731de83
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-31 21:00:31 +0530

    fixed issue w/sorting

--- client/src/store/products.js
+++ client/src/store/products.js
@@ -1,12 +1,26 @@
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../services/firebase";
 
-function products(state = { products: [] }, action) {
+function products(state = { products: [], message: "" }, action) {
   if (action.type === "UPDATE_DATA") {
-    return { ...state, products: [...action.payload] };
+    return { ...state, message: "", products: [...action.payload] };
+  }
+  if (action.type === "UPDATE_SORT") {
+    return { ...state, message: "", products: [...action.payload] };
+  }
+  if (action.type === "ERROR") {
+    return { ...state, products: [], message: action.payload };
+  }
+  if (action.type === "RESET") {
+    return { products: [], message: "" };
   }
   return state;
 }
+
+const sortedProduct = (elem) => {
+  return { type: "UPDATE_SORT", payload: elem };
+};
+
 const getProductsData = (id) => {
   return async (dispatch) => {
     try {
@@ -14,8 +28,14 @@ const getProductsData = (id) => {
       const docsSnap = await getDocs(subCollectionRef);
       let docs = docsSnap.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
       dispatch({ type: "UPDATE_DATA", payload: docs });
-    } catch (err) {}
+    } catch (err) {
+      dispatch({ type: "ERROR", payload: err.message });
+    }
   };
 };
 
-export { getProductsData, products };
+const resetProducts = () => {
+  return { type: "RESET" };
+};
+
+export { getProductsData, resetProducts, products, sortedProduct };
