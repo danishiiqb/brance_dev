c2d2760b1a41b11bb1ba8046946df6d7ea975201
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-31 20:59:59 +0530

    added sorting functionailty

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -7,41 +7,74 @@ import PaginationBtns from "../../components/DashBoard/MainInfo/PaginationBtns";
 import ProductTableRow from "../../components/DashBoard/MainInfo/ProductTableRow";
 import Search from "../../components/DashBoard/MainInfo/Search";
 import TableHeaderRow from "../../components/DashBoard/MainInfo/TableHeaderRow";
-import { getProductsData } from "../../store/products";
-import { reset } from "../../store/tableHeaderSortingReducer";
+import {
+  getProductsData,
+  resetProducts,
+  sortedProduct
+} from "../../store/products";
+import {
+  resettableHeader,
+  sortByType
+} from "../../store/tableHeaderSortingReducer";
 
 function Products() {
-  const { user, products } = useSelector((state) => {
-    return { user: state.user, products: state.products };
+  const { user, products, tableHeaderSorting } = useSelector((state) => {
+    return {
+      user: state.user,
+      products: state.products,
+      tableHeaderSorting: state.tableHeaderSorting
+    };
   });
+  const dispatcher = useDispatch();
+
   const [currPage, setCurrPage] = useState(1);
+
   function setActionWithId(obj) {
     console.log(obj);
   }
-  const headerRowStates = useSelector((state) => {
-    return state.tableHeaderSorting;
-  });
-  const dispatcher = useDispatch();
 
   function navigate(type) {
     setCurrPage((prev) => {
       return type === "forward" ? prev + 1 : prev - 1;
     });
   }
-
+  console.log(products);
   function pagination(page) {
     let start = (page - 1) * 8;
     let end = 8 * page;
     return products.products.slice(start, end);
   }
+
+  useEffect(() => {
+    return () => {
+      dispatcher(resettableHeader());
+      dispatcher(resetProducts());
+    };
+  }, [dispatcher]);
+
+  useEffect(() => {
+    if (tableHeaderSorting.modifiedArr.length > 0) {
+      dispatcher(sortedProduct(tableHeaderSorting.modifiedArr));
+    }
+  }, [tableHeaderSorting, dispatcher]);
+
+  useEffect(() => {
+    if (tableHeaderSorting.sortElemName && tableHeaderSorting.order) {
+      dispatcher(
+        sortByType(
+          products.products,
+          tableHeaderSorting.sortElemName.toUpperCase()
+        )
+      );
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [tableHeaderSorting.order, tableHeaderSorting.sortElemName, dispatcher]);
+
   useEffect(() => {
     if (user.user && user.user.type === "admin")
       dispatcher(getProductsData(user.user.uid));
   }, [dispatcher, user.user]);
 
-  useEffect(() => {
-    dispatcher(reset());
-  }, [dispatcher]);
   return (
     <div className={`h-panel `}>
       <div
@@ -51,6 +84,7 @@ function Products() {
           <Filter type="products"></Filter>
           <Search></Search>
         </div>
+
         {products.products.length > 0 ? (
           <table className="w-full my-3 ">
             <thead>
@@ -59,29 +93,23 @@ function Products() {
                   "Product Name",
                   "Category",
                   {
-                    name: "Date",
-                    order: headerRowStates.date
+                    name: "Date"
                   },
                   {
-                    name: "Price",
-                    order: headerRowStates.price
+                    name: "Price"
                   },
                   {
-                    name: "Stock",
-                    order: headerRowStates.stock
+                    name: "Stock"
                   },
                   {
-                    name: "Sold",
-                    order: headerRowStates.sold
+                    name: "Sold"
                   },
                   {
-                    name: "Revenue",
-                    order: headerRowStates.revenue
+                    name: "Revenue"
                   }
                 ]}
               ></TableHeaderRow>
             </thead>
-
             <tbody>
               {pagination(currPage).map((tableData, idx) => {
                 return (
@@ -95,12 +123,15 @@ function Products() {
               })}
             </tbody>
           </table>
+        ) : products.message !== "" ? (
+          <div className="flex absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+            <div className="text-[#FF385C]">{products.message}</div>
+          </div>
         ) : (
           <div className="flex absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
             <ImSpinner2 className="animate-spin fill-current text-[#FF385C] w-16 h-16"></ImSpinner2>
           </div>
         )}
-
         <PaginationBtns
           forwardDisable={8 * currPage >= products.products.length}
           backBtn={currPage === 1 ? true : false}
