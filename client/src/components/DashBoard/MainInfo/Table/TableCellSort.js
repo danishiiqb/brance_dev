df4d92ac743d72ac8c99965f2bb34e2e16919c59
Danish Iqbal <danishiiqbal82@gmail.com>
2022-01-31 21:01:35 +0530

    fixed errors added error action creator

--- client/src/components/DashBoard/MainInfo/Table/TableCellSort.js
+++ client/src/components/DashBoard/MainInfo/Table/TableCellSort.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 import { useDispatch } from "react-redux";
-import {
-  activateAscending,
-  activateDescending
-} from "../../../../store/tableHeaderSortingReducer";
+import { sortBy } from "../../../../store/tableHeaderSortingReducer";
+
 import StatusDropDown from "../StatusDropDown";
+
 function TableCellSort({ elem }) {
+  const [sortOrder, setSortOrder] = useState("asc");
   const dispatch = useDispatch();
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if (!firstRender.current) {
+      dispatch(sortBy(elem.name, sortOrder));
+    }
+    firstRender.current = false;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sortOrder, dispatch]);
+
   return (
     <div
       onClick={() => {
-        elem.order === "asc"
-          ? dispatch(activateDescending(elem.name.toLowerCase()))
-          : dispatch(activateAscending(elem.name.toLowerCase()));
+        setSortOrder((prev) => {
+          return prev === "asc" ? "desc" : "asc";
+        });
       }}
       className="flex space-x-0.5 items-center relative cursor-pointer"
     >
       <div>{elem.name}</div>
-      {elem.order === "desc" ? (
-        <IoMdArrowDropdown className="w-6  h-6 text-gray-400"></IoMdArrowDropdown>
-      ) : (
-        <IoMdArrowDropup className="w-6 h-6 text-gray-400"></IoMdArrowDropup>
-      )}
+      <div className="flex flex-col">
+        <IoMdArrowDropup className="w-5 h-5 text-[#b1b1b1]"></IoMdArrowDropup>
+        <IoMdArrowDropdown className="w-5 h-5 -mt-3 text-[#b1b1b1]"></IoMdArrowDropdown>
+      </div>
       {elem.name === "Status" && elem.order === "asc" && (
         <StatusDropDown></StatusDropDown>
       )}
