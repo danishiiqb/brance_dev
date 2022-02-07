c719ee8ca8cb95ce744bb1928f3c34478f65e0a3
Danish Iqbal <danishiiqbal82@gmail.com>
2022-02-08 04:07:50 +0530

    added expand dropdown filtering functionality

--- client/src/components/Shop/FilterType.js
+++ client/src/components/Shop/FilterType.js
@@ -1,7 +1,70 @@
 import React from "react";
+import { useState } from "react";
+import { ReactComponent as Dropdown } from "../../icons/dropdown.svg";
 
-function FilterType() {
-  return <div>ghgh</div>;
+function FilterType({ type }) {
+  const [dropdown, setDropdown] = useState(false);
+  function expandDeepDropdown() {
+    if (type.type !== "Style" && type.type !== "Pattern") {
+      return type.dropdownItems;
+    }
+    let excludedItems = [
+      "plain",
+      "print",
+      "stripe",
+      "embroidery",
+      "colour block",
+      "check",
+      "graphic",
+      "logo",
+      "other",
+      "slim"
+    ];
+    let dropItems = type.dropdownItems
+      .map((elem) => {
+        return Array.isArray(elem[type.type.toLowerCase()])
+          ? [...elem[type.type.toLowerCase()]]
+          : [];
+      })
+      .flat()
+      .filter((elem) => {
+        let element = elem.toLowerCase();
+        return !excludedItems.includes(element);
+      });
+    return type.type === "Pattern"
+      ? dropItems.concat(excludedItems.slice(0, excludedItems.length - 1))
+      : dropItems.concat(excludedItems.slice(-2));
+  }
+  return (
+    <div className="py-3 border-b-[1px] border-[#0000002f]">
+      <div
+        onClick={() => {
+          setDropdown((prev) => {
+            return !prev;
+          });
+        }}
+        className="flex  justify-between cursor-pointer items-center"
+      >
+        <div className="text-md font-medium">{type.type}</div>
+        <div>
+          <Dropdown className="w-2.5 h-2.5"></Dropdown>
+        </div>
+      </div>
+      <div className={`mt-1.5 ${dropdown ? "visible" : "hidden"}`}>
+        {dropdown && Array.isArray(type.dropdownItems)
+          ? expandDeepDropdown().map((elem) => {
+              return (
+                <div
+                  className={`py-[4px] animplane capitalize font-regular text-sm cursor-pointer`}
+                >
+                  {elem}
+                </div>
+              );
+            })
+          : "ghgh"}
+      </div>
+    </div>
+  );
 }
 
 export default FilterType;
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -4,6 +4,7 @@ import { useRef } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import FilterType from "../components/Shop/FilterType";
 import { db } from "../services/firebase";
 
 function Shop() {
@@ -11,16 +12,268 @@ function Shop() {
   const allProducts = useRef([]);
   let { id, type } = useParams();
   const filterType = useRef([
-    "Category",
-    "Sale/NewSeason",
-    "Size",
-    "Brand",
-    "Colour",
-    "Style",
-    "Pattern",
-    "Discount%",
-    "Price",
-    "Material"
+    {
+      type: "Category",
+      dropdownItems: [
+        "Jacket & Coats",
+        "Hoodies & SweatShirts",
+        "Tshirts & Polos",
+        "Joggers",
+        "Shirts",
+        "Jeans",
+        "LoungeWear",
+        "Pants & Chinos",
+        "Socks",
+        "Jumpers & Knitwear"
+      ]
+    },
+    {
+      type: "Sale/New Season",
+      dropdownItems: ["Stratnum Collection", "Diversity Collection"]
+    },
+    {
+      type: "Size",
+      dropdownItems: [
+        "x-small",
+        "small",
+        "medium",
+        "large",
+        "X-Large",
+        "XX-Large",
+        "3X-Large",
+        "4X-Large"
+      ]
+    },
+    {
+      type: "Brand",
+      dropdownItems: [
+        "Nike",
+        "Tommy Hilfiger",
+        "H&M",
+        "Adidas",
+        "Gap",
+        "North Face",
+        "Levis",
+        "Zara",
+        "Next",
+        "Urban Outfitters",
+        "Custom"
+      ]
+    },
+    {
+      type: "Colour",
+      dropdownItems: [
+        "Blue",
+        "Tommy Hilfiger",
+        "H&M",
+        "Adidas",
+        "Gap",
+        "North Face",
+        "Levis",
+        "Zara",
+        "Next",
+        "Urban Outfitters",
+        "Custom"
+      ]
+    },
+    {
+      type: "Style",
+      dropdownItems: [
+        {
+          name: "Jacket & Coats",
+          style: [
+            "Leather",
+            "OverCoat",
+            "PullOver",
+            "Biker",
+            "Bomber Jacket",
+            "Parkas",
+            "Denim Jacket",
+            "Track",
+            "Varsity",
+            "Wind Breaker",
+            "Other"
+          ]
+        },
+        {
+          name: "Hoodies & SweatShirts",
+          style: [
+            "Hooded",
+            "cuffed",
+            "zip through",
+            "tracksuit",
+            "oversized",
+            "other",
+            "joggers",
+            "fleece lined"
+          ]
+        },
+        {
+          name: "Tshirts & Polos",
+          style: [
+            "LongLine",
+            "Muscle",
+            "Oversized",
+            "slim fit",
+            "Regular",
+            "Relaxed",
+            "slim",
+            "Other"
+          ]
+        },
+        {
+          name: "Joggers",
+          style: ["cargo", "Cigarette", "Cropped", "slim", "relaxed", "other"]
+        },
+        {
+          name: "Shirts",
+          style: ["Denim", "Oxford", "Regular", "Relaxed", "Slim", "Other"]
+        },
+        {
+          name: "Jeans",
+          style: ["Slim", "Tapered", "Regular", "Other"]
+        },
+        {
+          name: "LoungeWear",
+          style: ["Hooded", "cuffed", "zip through", "jersey", "hoodies"]
+        },
+        {
+          name: "Pants & Chinos",
+          style: [
+            "cargo",
+            "Cigarette",
+            "chino",
+            "Cropped",
+            "slim",
+            "relaxed",
+            "other"
+          ]
+        },
+        {
+          name: "Socks",
+          style: ["ankle sock", "trainer sock", "slipper sock"]
+        },
+        {
+          name: "Sets & OutFits",
+          style: [
+            "Joggers sets",
+            "t-shirt set",
+            "sweater set",
+            "tracksuit",
+            "hooded",
+            "Co Ord"
+          ]
+        },
+        {
+          name: "Jumpers & KnitWear",
+          style: ["Oversized", "regular", "relaxed", "other"]
+        }
+      ]
+    },
+    {
+      type: "Pattern",
+      dropdownItems: [
+        {
+          name: "Jacket & Coats",
+          pattern: [
+            "plain",
+            "logo",
+            "stripe",
+            "check",
+            "print",
+            "graphic",
+            "embroidery",
+            "colour block",
+            "other"
+          ]
+        },
+        {
+          name: "Hoodies & SweatShirts",
+          pattern: [
+            "plain",
+            "logo",
+            "stripe",
+            "floral",
+            "check",
+            "print",
+            "graphic",
+            "embroidery",
+            "colour block",
+            "other"
+          ]
+        },
+        {
+          name: "Tshirts & Polos",
+          pattern: [
+            "logo",
+            "Plain",
+            "graphic",
+            "print",
+            "check",
+            "coulour block",
+            "other"
+          ]
+        },
+        {
+          name: "Joggers",
+          pattern: undefined
+        },
+        {
+          name: "Shirts",
+          pattern: [
+            "plain",
+            "check",
+            "print",
+            "stripe",
+            "logo",
+            "floral",
+            "tartan",
+            "other"
+          ]
+        },
+        {
+          name: "Jeans",
+          pattern: undefined
+        },
+        {
+          name: "LoungeWear",
+          pattern: undefined
+        },
+        {
+          name: "Pants & Chinos",
+          pattern: ["Plain", "check", "stripe", "logo", "other"]
+        },
+        {
+          name: "Socks",
+          pattern: ["plain", "logo", "print", "stripe", "embroidery", "other"]
+        },
+        {
+          name: "Sets & OutFits",
+          pattern: ["plain", "logo", "print", "stripe", "graphic", "other"]
+        },
+        {
+          name: "Jumpers & KnitWear",
+          pattern: [
+            "plain",
+            "logo",
+            "print",
+            "stripe",
+            "graphic",
+            "cable",
+            "colour block",
+            "other"
+          ]
+        }
+      ]
+    },
+    {
+      type: "Price",
+      dropdownItems: "slider"
+    },
+    {
+      type: "Material",
+      dropdownItems: ["Cotton", "Polyester", "Satin"]
+    }
   ]);
   useEffect(() => {
     async function getAllProducts() {
@@ -61,7 +314,11 @@ function Shop() {
             Clear Filters
           </div>
         </div>
-        <div>{}</div>
+        <div className="mt-2">
+          {filterType.current.map((type) => {
+            return <FilterType type={type} />;
+          })}
+        </div>
       </div>
       <div>hhhhh</div>
       {/* {products.map((el) => {
