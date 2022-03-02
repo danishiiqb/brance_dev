25beb5035e7b2dc8f9ccbe2b2cdc0185b87d4d16
Danish Iqbal <danishiiqbal82@gmail.com>
2022-03-02 05:42:10 +0530

    fixed errors

--- client/src/components/ProductDisplay/RatingSection.js
+++ client/src/components/ProductDisplay/RatingSection.js
@@ -4,15 +4,32 @@ import CustomerReviews from "./CustomerReviews";
 
 function RatingSection({ reviews }) {
   return (
-    <>
-      <div className="text-2xl mt-16 font-medium capitalize mb-5">
+    <div>
+      <div className="text-2xl font-medium capitalize mb-5">
         Customer Reviews
       </div>
-      <div className="flex space-x-20">
+      <div className="flex space-x-32">
         <CustomerReviews reviews={reviews}></CustomerReviews>
-        <Comments></Comments>
+        {reviews.length > 0 && (
+          <div className="flex-1 space-y-9">
+            <div className="space-y-9">
+              {reviews.map((rev, idx) => {
+                return rev.comment && rev.title ? (
+                  <Comments key={idx} review={rev}></Comments>
+                ) : (
+                  ""
+                );
+              })}
+            </div>
+            <div className="cursor-pointer text-center">
+              <div className="text-sm text-center text-gray-600 inline-block border-b-[1px] border-gray-600">
+                Read More Reviews
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
 
