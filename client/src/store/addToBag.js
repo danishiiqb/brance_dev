79f04679f2e5684fa0788c3329ac3e0c98c4670f
Danish Iqbal <danishiiqbal82@gmail.com>
2022-03-04 05:10:50 +0530

    added updating functionality

--- client/src/store/addToBag.js
+++ client/src/store/addToBag.js
@@ -1,6 +1,26 @@
 function addToBag(bag = [], action) {
   if (action.type === "ADD") {
-    return [...bag, action.payload];
+    return [
+      ...bag,
+      {
+        ...action.payload.data,
+        qty: action.payload.qty,
+        sizeSelected: action.payload.size
+      }
+    ];
+  }
+  if (action.type === "UPDATE") {
+    let updatedBag = bag.map((prod) => {
+      if (prod.id === action.payload.data.id) {
+        return {
+          ...action.payload.data,
+          qty: action.payload.qty,
+          sizeSelected: action.payload.size
+        };
+      }
+      return { ...prod };
+    });
+    return updatedBag;
   }
   if (action.type === "REMOVE") {
     let filtered = bag.filter((prod) => prod.id !== action.payload.id);
@@ -8,11 +28,28 @@ function addToBag(bag = [], action) {
   }
   return bag;
 }
-function add(payload) {
-  return { type: "ADD", payload };
+function add(payload, qty, size) {
+  return {
+    type: "ADD",
+    payload: {
+      data: payload,
+      qty,
+      size
+    }
+  };
+}
+function update(payload, qty, size) {
+  return {
+    type: "UPDATE",
+    payload: {
+      data: payload,
+      qty,
+      size
+    }
+  };
 }
 function remove(payload) {
   return { type: "REMOVE", payload };
 }
 
-export { addToBag, add, remove };
+export { addToBag, add, update, remove };
