cfb3d262d05f4b2978bd8820935957e93d68d770
Danish Iqbal <danishiiqbal82@gmail.com>
2022-03-04 05:11:55 +0530

    fixed issues

--- client/src/components/ProductDisplay/ProductRecomm.js
+++ client/src/components/ProductDisplay/ProductRecomm.js
@@ -1,74 +1,75 @@
-import { getDocs } from "firebase/firestore";
 import React from "react";
-import { useRef } from "react";
-import { useEffect } from "react";
 import Product from "../Product";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import { useState } from "react";
 
-function ProductRecomm({ products }) {
-  const track = useRef("");
-  const idx = useRef(0);
-  //   useEffect(() => {
-  //     console.log();
-  //   });
-
+function ProductRecomm({ products, title }) {
+  const [idx, setIdx] = useState(0);
   return (
     <div
       style={{
-        width: `calc((${window.innerWidth}px - 2 * ${"2.75rem"} - 16px)`
+        width: `calc((${window.innerWidth}px - 2 * ${"2.75rem"} - 1.5rem)`
       }}
       className="relative"
     >
+      <div className="text-2xl font-medium capitalize mb-5">
+        {title.toLowerCase()}
+      </div>
       <div className="overflow-hidden">
-        <div className="flex transition-all duration-200" ref={track}>
+        <div
+          style={{ transform: `translate(-${idx * 338}px)` }}
+          className="flex transition-all duration-300"
+        >
           {products.map((product) => {
             return (
               <div
                 key={product.id}
                 style={{
-                  width: `calc((${window.innerWidth}px - 2 * 2.75rem) / 5 - 16px)`
+                  width: `calc((${window.innerWidth}px - 2 * 2.75rem) / 4 - 1.5rem)`
                 }}
-                className={`flex-shrink-0 mr-4`}
+                className={`flex-shrink-0 mr-6`}
               >
-                <Product prodDesc={product} expandHeight={true}></Product>
+                <Product
+                  prodDesc={product}
+                  elongate={true}
+                  expandHeight={true}
+                ></Product>
               </div>
             );
           })}
         </div>
       </div>
       <div className="">
-        <div
-          onClick={() => {
-            if (idx.current >= products.length - 1) {
-              return;
-            }
-            idx.current += 1;
-            track.current.style.transform = `translate(-${
-              idx.current * 270
-            }px)`;
-          }}
-          className="absolute flex justify-center items-center bg-white w-12 h-12 rounded-full top-1/2 -translate-y-1/2 shadow-sm cursor-pointer"
-          style={{
-            right: `-24px`
-          }}
-        >
-          <IoIosArrowForward className="w-6 h-6"></IoIosArrowForward>
-        </div>
-        <div
-          onClick={() => {
-            if (idx.current <= 0) {
-              return;
-            }
-            idx.current -= 1;
-            track.current.style.transform = `translate(-${
-              idx.current * 270
-            }px)`;
-          }}
-          className="absolute flex justify-center items-center bg-white w-12 h-12 rounded-full top-1/2 -translate-y-1/2 shadow-sm cursor-pointer"
-          style={{ left: `-24px` }}
-        >
-          <IoIosArrowBack className="w-6 h-6"></IoIosArrowBack>
-        </div>
+        {products.length > 5 && idx !== products.length - 4 && (
+          <div
+            onClick={() => {
+              if (idx >= products.length - 1) {
+                return;
+              }
+              setIdx((prev) => prev + 1);
+            }}
+            className="absolute transition-all duration-150  hover:shadow-sm_dark flex justify-center items-center bg-white group w-12 h-12 rounded-full top-1/2 -translate-y-1/2 shadow-sm cursor-pointer"
+            style={{
+              right: `-24px`
+            }}
+          >
+            <IoIosArrowForward className="w-6 group-hover:fill-black h-6"></IoIosArrowForward>
+          </div>
+        )}
+        {idx > 0 && (
+          <div
+            onClick={() => {
+              if (idx <= 0) {
+                return;
+              }
+              setIdx((prev) => prev - 1);
+            }}
+            className="absolute hover:shadow-sm_dark flex justify-center items-center bg-white w-12 h-12 rounded-full transition-all duration-150 top-1/2 -translate-y-1/2 shadow-sm cursor-pointer"
+            style={{ left: `-24px` }}
+          >
+            <IoIosArrowBack className="w-6 h-6"></IoIosArrowBack>
+          </div>
+        )}
       </div>
     </div>
   );
