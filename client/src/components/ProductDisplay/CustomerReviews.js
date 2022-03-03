b9cae0dce8a538a099b2bddd803b7ac18f798e03
Danish Iqbal <danishiiqbal82@gmail.com>
2022-03-04 05:03:14 +0530

    fixed bugs

--- client/src/components/ProductDisplay/CustomerReviews.js
+++ client/src/components/ProductDisplay/CustomerReviews.js
@@ -6,14 +6,17 @@ function CustomerReviews({ reviews }) {
     let totalStars = 5;
     let filled = [...Array(Math.floor(rating))].map((_) => {
       return (
-        <BsStarFill className="w-[1.43rem] h-[1.43rem] fill-current text-[#FFC107]" />
+        <BsStarFill
+          key={Math.random()}
+          className="w-[1.43rem] h-[1.43rem] fill-current text-[#FFC107]"
+        />
       );
     });
     let empty = [];
     if (rating % 1 === 0) {
       empty = [...Array(totalStars - rating)].map((_, idx) => (
         <BsStarFill
-          key={idx}
+          key={Math.random()}
           className={`w-[1.43rem] h-[1.43rem] fill-current text-[#ECEFF1]`}
         />
       ));
@@ -21,12 +24,12 @@ function CustomerReviews({ reviews }) {
       empty = [...Array(totalStars - Math.floor(rating))].map((_, idx) => {
         return idx === 0 ? (
           <BsStarHalf
-            key={idx}
+            key={Math.random()}
             className={`w-[1.43rem] h-[1.43rem] text-[#FFC107]`}
           />
         ) : (
           <BsStarFill
-            key={idx}
+            key={Math.random()}
             className={`w-[1.43rem] h-[1.43rem]
             fill-current text-[#ECEFF1]`}
           />
@@ -37,22 +40,36 @@ function CustomerReviews({ reviews }) {
   }
   function noOfRatings(num) {
     let filteredReviews = reviews.filter(
-      (rev) => rev.rating >= num && rev.rating <= num
+      (rev) => Math.round(rev.rating) === num
     );
     return filteredReviews.length;
   }
   return (
-    <div className="space-y-6 w-[22%]">
+    <div className="space-y-6 w-[25%]">
       <div className="space-y-2">
         <div className="flex space-x-2.5 justify-center bg-[#FF385C] p-4 px-8 items-center">
           <div className="flex space-x-1">
             {renderStars(
-              reviews.reduce((prev, curr) => {
-                return prev + curr.rating;
-              }, 0) / reviews.length
+              reviews.length > 0
+                ? reviews.reduce((prev, curr) => {
+                    return prev + curr.rating;
+                  }, 0) / reviews.length
+                : 0
             )}
           </div>
-          <div className="font-medium text-2xl">4.8</div>
+          <div className="font-medium text-2xl">
+            {reviews.length > 0
+              ? (
+                  Math.round(
+                    (reviews.reduce((prev, curr) => {
+                      return prev + curr.rating;
+                    }, 0) /
+                      reviews.length) *
+                      100
+                  ) / 100
+                ).toFixed(1)
+              : 0}
+          </div>
         </div>
         <div className="text-center text-md">({reviews.length}) Reviews</div>
       </div>
