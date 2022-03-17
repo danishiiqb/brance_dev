dd931bf139a6e4a0581eb68718a926c565b785b4
Danish Iqbal <danishiiqbal82@gmail.com>
2022-03-17 23:57:01 +0530

    added no of liked product functionailty

--- client/src/components/LikedProduct.js
+++ client/src/components/LikedProduct.js
@@ -20,6 +20,12 @@ function LikedProduct({ likedProd }) {
         filledicon={RiHeartFill}
         icon={RiHeartLine}
       ></Icons>
+      {likedProd.length > 0 && (
+        <div className="absolute -top-0.5 w-4 h-4 flex justify-center items-center text-[10px] font-medium rounded-full text-white bg-[#FF385C] left-[63%] select-none ">
+          {likedProd.length}
+        </div>
+      )}
+
       {expand && <BagReview liked={true} addToProd={likedProd}></BagReview>}
     </div>
   );
