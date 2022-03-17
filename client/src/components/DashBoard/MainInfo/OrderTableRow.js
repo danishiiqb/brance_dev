06cfa7564f336f6a3a60df6c9bd860dbe302c241
Danish Iqbal <danishiiqbal82@gmail.com>
2022-03-17 23:54:35 +0530

    added convert date functionality

--- client/src/components/DashBoard/MainInfo/OrderTableRow.js
+++ client/src/components/DashBoard/MainInfo/OrderTableRow.js
@@ -3,28 +3,48 @@ import Info from "./Info";
 import TableData from "./Table/TableData";
 import TableRow from "./Table/TableRow";
 
-function OrderTableRow({ Id, setActionWithId, tableData, type }) {
+function OrderTableRow({ Id, setActionWithId, edit, detailedItem, type }) {
   function setAction(type) {
     setActionWithId({ type, Id });
   }
+  function convertDate(sec) {
+    let date = new Date(sec * 1000);
+    return `${date.getDate()}/${
+      date.getMonth() + 1 > 9
+        ? `${date.getMonth() + 1}`
+        : `0${date.getMonth() + 1}`
+    }/${date.getFullYear()}`;
+  }
+  function shortenTitle(title) {
+    if (title.length > 24) {
+      return `${title.substr(0, 25)}..`.toLowerCase();
+    }
+    return title.toLowerCase();
+  }
+
   return (
     <TableRow>
-      <TableData>{tableData.orderId}</TableData>
-      <TableData>{tableData.user}</TableData>
-      <TableData>{tableData.address}</TableData>
-      <TableData>{tableData.date}</TableData>
+      <TableData>{detailedItem.id.split("-")[0]}</TableData>
+      <TableData>{detailedItem.name}</TableData>
       <TableData>
-        <div className="font-medium">${tableData.amount}.00</div>
+        <span className=" capitalize ">
+          {shortenTitle(detailedItem.details.title.toLowerCase())}
+        </span>{" "}
       </TableData>
+      <TableData>
+        {detailedItem.shipping.line1 || detailedItem.shipping.line2}
+      </TableData>
+      <TableData>{convertDate(detailedItem.timestamp.seconds)}</TableData>
+      <TableData>${detailedItem.prize.toFixed(2)}</TableData>
       <TableData>
         <div className="flex items-center space-x-4">
           <div className="bg-yellow-200 flex-1 py-1.5 text-center text-sm font-semibold  rounded-lg">
-            {tableData.status}
+            {detailedItem.status}
           </div>
         </div>
       </TableData>
       <TableData className="px-1.5 py-2.5">
-        <Info clickedAction={setAction} type={type}></Info>
+        <Info setEditPhase={edit} clickedAction={setAction} type={type}></Info>
       </TableData>
     </TableRow>
   );
