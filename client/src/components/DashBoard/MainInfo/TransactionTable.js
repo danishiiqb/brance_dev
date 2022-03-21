6316f7818610f08839e868a20a4a2548eaa5ef90
Danish Iqbal <danishiiqbal82@gmail.com>
2022-03-21 23:49:31 +0530

    connected to db to get data

--- client/src/components/DashBoard/MainInfo/TransactionTable.js
+++ client/src/components/DashBoard/MainInfo/TransactionTable.js
@@ -3,19 +3,26 @@ import Info from "./Info";
 import TableData from "./Table/TableData";
 import TableRow from "./Table/TableRow";
 
-function TransactionTable({ Id, setActionWithId, tableData, type }) {
+function TransactionTable({ filterByDate, setActionWithId, tableData, type }) {
   function setAction(type) {
-    setActionWithId({ type, Id });
+    setActionWithId({ type });
   }
+
   return (
     <TableRow>
       <TableData>{tableData.transactionId}</TableData>
-      <TableData>{tableData.amount}</TableData>
-      <TableData>08/07/22, 16:05</TableData>
+      <TableData>
+        <div className=" capitalize">{tableData.name.toLowerCase()}</div>
+      </TableData>
+      <TableData>${tableData.amount}</TableData>
+      <TableData>{tableData.date}</TableData>
       <TableData>
         <div className="flex items-center space-x-2.5">
           <div>{tableData.method}</div>
         </div>
+      </TableData>
+      <TableData>
+        <div className=" capitalize">{tableData.payment}</div>
       </TableData>
       <TableData className="px-1.5 py-2.5">
         <Info clickedAction={setAction} type={type}></Info>
