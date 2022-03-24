b91bad5fb2ba156fa0ce623a7947987fc701e3f2
Danish Iqbal <danishiiqbal82@gmail.com>
2022-03-24 23:23:02 +0530

    added convertDate and renderStars functonality

--- client/src/pages/Admin/Reviews.js
+++ client/src/pages/Admin/Reviews.js
@@ -1,25 +1,26 @@
 import { collection, getDocs } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { ImSpinner2 } from "react-icons/im";
 import { useSelector } from "react-redux";
 import Filter from "../../components/DashBoard/MainInfo/Filter";
-import PaginationBtns from "../../components/DashBoard/MainInfo/PaginationBtns";
+
 import ReviewsTableRowWrapper from "../../components/DashBoard/MainInfo/Reviews/ReviewsTableRowWrapper";
 import Search from "../../components/DashBoard/MainInfo/Search";
 import { db } from "../../services/firebase";
 
 function Reviews() {
   const [reviews, setReviews] = useState([]);
+  const [message, setMessage] = useState("");
+  const reviewsCopy = useRef([]);
   const [reload, setRelaod] = useState(false);
-  const { user, products, tableHeaderSorting, filteredData } = useSelector(
-    (state) => {
-      return {
-        user: state.user,
-        products: state.products,
-        tableHeaderSorting: state.tableHeaderSorting,
-        filteredData: state.filteredData
-      };
-    }
-  );
+  const { user } = useSelector((state) => {
+    return {
+      user: state.user,
+      products: state.products,
+      tableHeaderSorting: state.tableHeaderSorting,
+      filteredData: state.filteredData
+    };
+  });
 
   useEffect(() => {
     async function getReviews() {
@@ -37,32 +38,66 @@ function Reviews() {
           reviewsArr.push({ ...item.data(), id: item.id });
         }
       });
-      setReviews(reviewsArr);
+      setReviews(
+        [...reviewsArr].sort(
+          (a, b) => b.createdAt.seconds - a.createdAt.seconds
+        )
+      );
+      reviewsCopy.current = reviewsArr;
       reload && setRelaod(false);
     }
     (user.user || reload) && getReviews();
-  }, [user.user]);
+  }, [user.user, reload]);
 
+  function filterByDate(start, end) {
+    const filteredData = reviewsCopy.current.filter((product) => {
+      let d1 = new Date(product.createdAt.seconds * 1000);
+      let d2 = start;
+      let d3 = end;
+      return d2.toDateString() !== d3.toDateString()
+        ? d1 >= d2 && (d1 <= d3 || d1.toDateString() === d3.toDateString())
+        : d1.toDateString() === d2.toDateString();
+    });
+    setReviews(filteredData);
+    filteredData.length === 0 && setMessage("No Reviews found");
+  }
   return (
-    <div className="">
-      <div className="bg-white h-full shadow-sm_dark rounded-md mt-6 p-small">
-        <div className="px-1.5 flex items-center justify-between">
-          <Filter type="reviews"></Filter>
+    <div className="h-panel">
+      <div className="bg-white overflow-scroll h-full  no-scrollbar relative shadow-sm_dark rounded-md mt-6 p-small">
+        <div className="px-1.5 flex items-center relative z-50 justify-between">
+          <Filter
+            filterResetParent={() => {
+              setReviews(reviewsCopy.current);
+            }}
+            custom={true}
+            filterByDate={filterByDate}
+            type="reviews"
+          ></Filter>
           <Search></Search>
         </div>
         <div className="mt-5">
-          <div className=" space-y-4">
-            {reviews.map((tableData, idx) => {
-              return (
-                <ReviewsTableRowWrapper
-                  setReload={setRelaod}
-                  type="reviews"
-                  key={idx}
-                  admin={{ name: user.user.displayName, uid: user.user.uid }}
-                  tableData={tableData}
-                ></ReviewsTableRowWrapper>
-              );
-            })}
+          <div className="space-y-5">
+            {reviews.length > 0 ? (
+              reviews.map((tableData, idx) => {
+                return (
+                  <ReviewsTableRowWrapper
+                    setReload={setRelaod}
+                    type="reviews"
+                    key={idx}
+                    admin={{ name: user.user.displayName, uid: user.user.uid }}
+                    tableData={tableData}
+                  ></ReviewsTableRowWrapper>
+                );
+              })
+            ) : message ? (
+              <div className="flex absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+                <div className="text-[#FF385C]">{message}</div>
+              </div>
+            ) : (
+              <div className="flex absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+                <ImSpinner2 className="animate-spin fill-current text-[#FF385C] w-16 h-16"></ImSpinner2>
+              </div>
+            )}
           </div>
         </div>
       </div>
