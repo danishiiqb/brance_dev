74546def8d798e717df6882b699e112e37fd5a4f
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-01 23:32:13 +0530

    fixed ordertableRow changed styling

--- client/src/components/DashBoard/MainInfo/OrderTableRow.js
+++ client/src/components/DashBoard/MainInfo/OrderTableRow.js
@@ -3,10 +3,15 @@ import Info from "./Info";
 import TableData from "./Table/TableData";
 import TableRow from "./Table/TableRow";
 
-function OrderTableRow({ Id, setActionWithId, edit, detailedItem, type }) {
-  function setAction(type) {
-    setActionWithId({ type, Id });
-  }
+function OrderTableRow({
+  edit,
+  view,
+  showBread,
+  detailedItem,
+  deleteItem,
+  type,
+  modal
+}) {
   function convertDate(sec) {
     let date = new Date(sec * 1000);
     return `${date.getDate()}/${
@@ -35,17 +40,36 @@ function OrderTableRow({ Id, setActionWithId, edit, detailedItem, type }) {
         {detailedItem.shipping.line1 || detailedItem.shipping.line2}
       </TableData>
       <TableData>{convertDate(detailedItem.timestamp.seconds)}</TableData>
-      <TableData>${detailedItem.prize.toFixed(2)}</TableData>
+      <TableData>${(detailedItem.prize + 5.99).toFixed(2)}</TableData>
       <TableData>
         <div className="flex items-center space-x-4">
-          <div className="bg-yellow-200 flex-1 py-1.5 text-center text-sm font-semibold  rounded-lg">
+          <div
+            className={`${
+              detailedItem.status === "Pending"
+                ? "bg-yellow-200"
+                : detailedItem.status === "Shipped"
+                ? "bg-green-200"
+                : detailedItem.status === "Out for Delivery"
+                ? "bg-green-400"
+                : detailedItem.status === "Delivered"
+                ? "bg-red-400"
+                : ""
+            }   flex-1 py-1.5 text-center text-sm font-semibold  rounded-sm`}
+          >
             {detailedItem.status}
           </div>
         </div>
       </TableData>
-      <TableData className="px-1.5 py-2.5">
-        <Info setEditPhase={edit} clickedAction={setAction} type={type}></Info>
-      </TableData>
+      {!modal && !showBread && (
+        <TableData className="px-1.5 py-2.5">
+          <Info
+            setDelete={deleteItem}
+            setEditPhase={edit}
+            setViewPhase={view}
+            type={type}
+          ></Info>
+        </TableData>
+      )}
     </TableRow>
   );
 }
