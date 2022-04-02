5e109b85bc074ffc0460e6300f418f14285cbc99
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-02 23:58:43 +0530

    fixed graph.js month issue and props

--- client/src/components/DashBoard/MainInfo/Graph.js
+++ client/src/components/DashBoard/MainInfo/Graph.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 
-function Graph() {
+function Graph({ orders, visitors }) {
   return (
     <div className="p-small cursor-pointer  w-8/12  relative rounded-md shadow-sm_dark bg-white">
       <h3 className="font-medium text-lg mb-2">Sale Statistics</h3>
@@ -10,20 +10,33 @@ function Graph() {
           width={30}
           height={17}
           data={{
-            labels: ["January", "February", "March", "April", "May", "June"],
+            labels: [
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+              "August",
+              "September",
+              "October",
+              "November",
+              "December"
+            ],
             datasets: [
               {
                 label: "Sales",
                 backgroundColor: "rgb(255, 99, 132)",
                 borderColor: "rgb(250, 12, 100)",
-                data: [0, 10, 5, 2, 20, 30, 45],
+                data: [...orders],
                 fill: true,
                 borderWidth: 5,
                 hoverBackgroundColor: "rgb(200, 99, 132)"
               },
               {
                 label: "Visitors",
-                data: [0, 10, 5, 2, 20, 70, 499],
+                data: [...visitors],
                 borderColor: "rgb(53, 162, 235)",
                 backgroundColor: "rgba(53, 162, 235, 0.5)"
               }
