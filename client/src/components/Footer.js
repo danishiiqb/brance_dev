72eb999900f98c1e339ae0482fc3d7dbf800e8bc
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-05 23:41:39 +0530

    made minor changes to footer annd brands coponent

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,14 +3,14 @@ import React from "react";
 function Footer() {
   return (
     <footer
-      className="mt-14 py-2 text-white bg-[#FF385C]"
+      className="mt-14 py-2 w-full text-white bg-[#FF385C]"
       style={{
         textAlign: "center",
         fontSize: "1rem",
         fontWeight: "bold"
       }}
     >
-      &copy; Made By Danish Iqbal
+      &copy; Designed and Developed by Danish Iqbal
     </footer>
   );
 }
