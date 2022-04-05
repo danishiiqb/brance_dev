53d0421057107c43b138f686f36d92d385241de1
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-05 23:38:24 +0530

    added InfoModal for displying details of products

--- client/src/pages/Admin/Products.js
+++ client/src/pages/Admin/Products.js
@@ -3,15 +3,22 @@ import { useState } from "react";
 import { ImSpinner2 } from "react-icons/im";
 import { useDispatch, useSelector } from "react-redux";
 import Filter from "../../components/DashBoard/MainInfo/Filter";
+import InfoModal from "../../components/DashBoard/MainInfo/Modals/InfoModal";
 import PaginationBtns from "../../components/DashBoard/MainInfo/PaginationBtns";
 import ProductTableRow from "../../components/DashBoard/MainInfo/ProductTableRow";
 import Search from "../../components/DashBoard/MainInfo/Search";
 import TableHeaderRow from "../../components/DashBoard/MainInfo/TableHeaderRow";
 import { filterCategory, filterDate } from "../../store/filteredData";
-import { getProductsData, updateProduct } from "../../store/products";
-import { sortByType } from "../../store/tableHeaderSortingReducer";
+import { getProductsData, updateProduct } from "../../store/productStore";
+import {
+  resettableHeader,
+  sortByType
+} from "../../store/tableHeaderSortingReducer";
 
 function Products() {
+  const [modalDta, setModalData] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const firstRender = useRef(false);
   const { user, products, tableHeaderSorting, filteredData } = useSelector(
     (state) => {
       return {
@@ -25,12 +32,7 @@ function Products() {
 
   const referProducts = useRef(null);
   const dispatcher = useDispatch();
-
   const [currPage, setCurrPage] = useState(1);
-
-  function setActionWithId(obj) {
-    console.log(obj);
-  }
 
   function navigate(type) {
     setCurrPage((prev) => {
@@ -43,15 +45,23 @@ function Products() {
     let end = 8 * page;
     return products.products.slice(start, end);
   }
-
   useEffect(() => {
-    if (tableHeaderSorting.modifiedArr.length > 0) {
+    return () => {
+      dispatcher(resettableHeader());
+    };
+  }, []);
+  useEffect(() => {
+    if (tableHeaderSorting.modifiedArr.length > 0 && firstRender.current) {
       dispatcher(updateProduct(tableHeaderSorting.modifiedArr));
     }
   }, [tableHeaderSorting, dispatcher]);
 
   useEffect(() => {
-    if (tableHeaderSorting.sortElemName && tableHeaderSorting.order) {
+    if (
+      tableHeaderSorting.sortElemName &&
+      tableHeaderSorting.order &&
+      firstRender.current
+    ) {
       dispatcher(
         sortByType(
           products.products,
@@ -64,7 +74,7 @@ function Products() {
 
   useEffect(() => {
     if (user.user && user.user.type === "admin")
-      dispatcher(getProductsData(user.user.uid, "products"));
+      dispatcher(getProductsData(user.user.uid));
   }, [dispatcher, user.user]);
 
   useEffect(() => {
@@ -74,7 +84,7 @@ function Products() {
   }, [products]);
 
   useEffect(() => {
-    if (referProducts.current) {
+    if (firstRender.current) {
       setCurrPage(1);
       if (
         typeof filteredData === "object" &&
@@ -86,10 +96,11 @@ function Products() {
         if (filteredData.length > 0) {
           dispatcher(updateProduct(filteredData));
         } else if (filteredData.length === 0) {
-          dispatcher({ type: "ERROR", payload: "No Products found" });
+          dispatcher({ type: "PROD_ERROR", payload: "No Products found" });
         }
       }
     }
+    firstRender.current = true;
   }, [filteredData, dispatcher]);
 
   function filterByDate(start, end) {
@@ -100,11 +111,11 @@ function Products() {
     dispatcher(filterCategory({ type, elementsArr: referProducts.current }));
   }
   return (
-    <div className={`h-panel`}>
+    <div className={`h-panel  relative`}>
       <div
-        className={`bg-white  relative  h-full shadow-sm_dark rounded-md mt-6 p-small`}
+        className={`bg-white flex-1  relative  h-full shadow-sm_dark rounded-md mt-6 p-small`}
       >
-        <div className="px-1.5 flex items-center justify-between">
+        <div className="px-1.5 flex relative z-50  items-center justify-between">
           <Filter
             filterByDate={filterByDate}
             filterByCategory={filterByCategory}
@@ -142,7 +153,15 @@ function Products() {
               {pagination(currPage).map((tableData, idx) => {
                 return (
                   <ProductTableRow
-                    setActionWithId={setActionWithId}
+                    viewFunc={(values) => {
+                      setModalData(values);
+                    }}
+                    // deleteFunc={deleteOrders}
+                    modal={modalDta}
+                    editFunc={(values) => {
+                      setModalData(values);
+                      setEditMode(true);
+                    }}
                     type="products"
                     key={tableData.id}
                     tableData={tableData}
@@ -152,7 +171,7 @@ function Products() {
             </tbody>
           </table>
         ) : products.message !== "" ? (
-          <div className="flex absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+          <div className="flex absolute font-medium text-lg  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
             <div className="text-[#FF385C]">{products.message}</div>
           </div>
         ) : (
@@ -160,7 +179,6 @@ function Products() {
             <ImSpinner2 className="animate-spin fill-current text-[#FF385C] w-16 h-16"></ImSpinner2>
           </div>
         )}
-
         <PaginationBtns
           forwardDisable={8 * currPage >= products.products.length}
           backBtn={currPage === 1 ? true : false}
@@ -168,6 +186,23 @@ function Products() {
           navigate={navigate}
         ></PaginationBtns>
       </div>
+      {modalDta && (
+        <div className="fixed z-50 w-[120%]  top-0  -left-5   h-full bg-[#00000073]"></div>
+      )}
+      {modalDta && (
+        <div className=" z-50 w-[62%]  absolute h-max -translate-x-1/2 -translate-y-1/2 top-[50%] left-1/2  mt-6">
+          <InfoModal
+            notifyParent={() => {
+              setModalData(null);
+              editMode && setEditMode(false);
+            }}
+            type="Products"
+            modalDta={modalDta}
+            editMode={editMode}
+            user={user.user}
+          />
+        </div>
+      )}
     </div>
   );
 }
