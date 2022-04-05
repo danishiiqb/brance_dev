5e9222d49f8ec13d8fca2dbc46397ba0f5aaea9c
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-05 23:39:07 +0530

    fixed sidebar issue

--- client/src/components/DashBoard/SideBar/SideBarElem.js
+++ client/src/components/DashBoard/SideBar/SideBarElem.js
@@ -1,21 +1,23 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { resetProducts } from "../../../store/products";
-import { resettableHeader } from "../../../store/tableHeaderSortingReducer";
+import { Redirect, useHistory, useLocation } from "react-router-dom";
+import { auth } from "../../../services/firebase";
 
-function SideBarElem({ item, activatedElem, activate }) {
+function SideBarElem({ item }) {
   const [hover, setHover] = useState(false);
   const history = useHistory();
-  const dispatch = useDispatch();
+  const params = useLocation();
+  let url = params.pathname.split("/")[2];
 
-  return (
+  return url ? (
     <div
       onClick={() => {
-        activate(item.name);
-        dispatch(resetProducts());
-        dispatch(resettableHeader());
+        if (item.name.toLowerCase() === "logout") {
+          signOut(auth);
+          history.push(`/`);
+          return;
+        }
         history.push(`/admin/${item.name.toLowerCase()}`);
       }}
       onMouseEnter={() => {
@@ -26,18 +28,20 @@ function SideBarElem({ item, activatedElem, activate }) {
       }}
       className={`flex items-center py-2 space-x-3.5 relative
  after:h-full hover:after:opacity-100  after:bg-[#ff385d23] after:transition-all after:rounded-full after:duration-500 after:absolute ${
-   activatedElem === item.name
-     ? "text-[#FF385C] after:opacity-100  after:w-[271px]"
+   url.toLowerCase() === item.name.toLowerCase()
+     ? "text-[#FF385C] after:opacity-100  after:w-[256px]"
      : "after:opacity-0"
  } hover:text-[#FF385C] hover:after:w-[270px] after:w-[20px] after:-left-2/3 after:-z-1 z-20 cursor-pointer transition-all duration-200`}
     >
-      {hover || activatedElem === item.name ? (
+      {hover || url.toLowerCase() === item.name.toLowerCase() ? (
         <item.hover className="w-6 h-6 transition-all duration-75"></item.hover>
       ) : (
         <item.icon className="w-6 h-6"></item.icon>
       )}
       <div>{item.name}</div>
     </div>
+  ) : (
+    <Redirect to="/admin/dashboard" />
   );
 }
 
