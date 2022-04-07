32b1ee1e540031484ca4996e81f8c550931d48a8
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-07 23:44:51 +0530

    added links for different hero sectioons

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { ReactComponent as Bag } from "../icons/bag.svg";
 import gsap from "gsap";
 import useWindow from "../hooks/useWindow";
+import { useHistory } from "react-router-dom";
 
 function Hero({ modalOpen }) {
   const timeline = useRef(gsap.timeline());
@@ -11,29 +12,33 @@ function Hero({ modalOpen }) {
       heading: "Stratnum/Collection",
       logo: "/img/logos/levis.png",
       para: "New Collection for Winters",
-      img: "/img/banner1.png"
+      img: "/img/banner1.png",
+      link: "/shop/mens/brand/levis"
     },
     {
       heading: "Diversity/Collection",
       logo: "/img/logos/vans.png",
       para: "Be diverse with Vans diversity collection",
-      img: "/img/banner2.png"
+      img: "/img/banner2.png",
+      link: "/shop/mens/brand/vans"
     },
     {
       heading: "AIR JORDAN/BUCKS-23",
       logo: "/img/logos/nike.png",
       para: "Be a force with bucks-23 by nike",
-      img: "/img/banner3.png"
+      img: "/img/banner3.png",
+      link: "/shop/mens/brand/nike"
     },
     {
       heading: "COLLUSION/BEANIE- 45",
       logo: "/img/logos/collusion.png",
       para: "Wear with style in winters",
-      img: "/img/banner4.png"
+      img: "/img/banner4.png",
+      link: "/shop/mens/brand/champion"
     }
   ]);
   const [idx, setIdx] = useState(0);
-
+  const history = useHistory();
   useEffect(() => {
     let time;
 
@@ -112,7 +117,7 @@ function Hero({ modalOpen }) {
           <h1 className="font-extrabold leading-7 uppercase text-2xl lg:text-8xl">
             {heroInfo[idx].heading.split("/").map((elem, idx) => {
               return (
-                <div className="overflow-hidden">
+                <div key={idx} className="overflow-hidden">
                   <span
                     className={`head block transform translate-y-24 ${
                       idx === heroInfo[idx].heading.split("/").length - 1
@@ -131,6 +136,9 @@ function Hero({ modalOpen }) {
           </p>
           {width > 700 ? (
             <button
+              onClick={() => {
+                history.push(heroInfo[idx].link);
+              }}
               className={`main-btn hover:scale-x-105 opacity-0 translate-y-6 hover:pr-14 transform group bg-black shadow-dark after:absolute hover:after:h-full hover:after:opacity-100 hover:text-black after:-z-1 z-0  after:transition-all after:duration-500 after:w-full after:opacity-0 after:bg-white after:top-0 after:left-0 after:rounded-lg after:h-2 transition-all duration-200  text-white px-11 mt-9  hover:shadow-darkest py-4 font-bold  text-xl rounded-lg  relative`}
             >
               <span className=" .backface-hidden group-hover:-ml-1.5 relative group-hover:mr-1 transition-all duration-100">
@@ -139,7 +147,12 @@ function Hero({ modalOpen }) {
               <Bag className="w-8 h-8 group-hover:opacity-100 group-hover:animate-wiggle absolute right-6 top-3.5 transition-all fill-current text-transparent  duration-300 opacity-0 peer"></Bag>
             </button>
           ) : (
-            <button className="bg-black shadow-dark text-[6px] text-white px-1 mt-2  rounded-sm font-bold py-1">
+            <button
+              onClick={() => {
+                history.push(heroInfo[idx].link);
+              }}
+              className="bg-black shadow-dark text-[6px] text-white px-1 mt-2  rounded-sm font-bold py-1"
+            >
               Shop Now
             </button>
           )}
