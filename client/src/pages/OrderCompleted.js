655c96870670b44564fd0a2bf559ccc22cdb0c90
Danish Iqbal <danishiiqbal82@gmail.com>
2022-04-08 17:03:27 +0530

    minor changes

--- client/src/pages/OrderCompleted.js
+++ client/src/pages/OrderCompleted.js
@@ -1,6 +1,7 @@
 import { doc, getDoc } from "firebase/firestore";
 import React, { useEffect, useRef, useState } from "react";
 import { BsBagCheckFill } from "react-icons/bs";
+import { ImSpinner2 } from "react-icons/im";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import WrapperOrders from "../components/OrderSucces/WrapperOrders";
@@ -9,6 +10,7 @@ import { db } from "../services/firebase";
 function OrderCompleted() {
   const { user } = useSelector((state) => state.user);
   const [compOrder, setCompOrder] = useState("");
+
   const divBox = useRef("");
   const history = useHistory();
   useEffect(() => {
@@ -35,7 +37,7 @@ function OrderCompleted() {
   }, []);
 
   return (
-    <div className="px-11  mt-8">
+    <div className="px-11 relative max-w-screen-2xl m-auto min-h-screen  mt-8">
       <div
         ref={divBox}
         className="flex cursor-pointer fade  bg-green-200 justify-center rounded-md w-[70%] p-4  m-auto items-center space-x-2"
@@ -48,19 +50,26 @@ function OrderCompleted() {
         </div>
       </div>
       <div className="mt-12">
-        {compOrder && <WrapperOrders order={compOrder} />}
-      </div>
-
-      <div className="mt-2 text-center">
-        <button
-          onClick={() => {
-            history.replace("/MyOrders");
-          }}
-          className="text-xs after:bg-black relative after:top-[105%] after:left-0 after:absolute after:w-full after:h-[1px] hover:after:bg-[#FF385C]  hover:text-[#FF385C]"
-          to="/MyOrders"
-        >
-          See all Orders
-        </button>
+        {compOrder ? (
+          <>
+            <WrapperOrders order={compOrder} />
+            <div className="mt-2 text-center">
+              <button
+                onClick={() => {
+                  history.replace("/MyOrders");
+                }}
+                className="text-xs after:bg-black relative after:top-[105%] after:left-0 after:absolute after:w-full after:h-[1px] hover:after:bg-[#FF385C]  hover:text-[#FF385C]"
+                to="/MyOrders"
+              >
+                See all Orders
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="flex absolute  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+            <ImSpinner2 className="animate-spin fill-current text-[#FF385C] w-16 h-16"></ImSpinner2>
+          </div>
+        )}
       </div>
     </div>
   );
