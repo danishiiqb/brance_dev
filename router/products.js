3e955d7819be7d0a1eb8d01601a6b05c273c50c8
Danish Iqbal <96720314+danishiiqb@users.noreply.github.com>
2022-04-25 15:19:16 +0530

    Update products.js

--- router/products.js
+++ router/products.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import {
-  deleteProducts,
-  getAllProducts,
-  postProducts,
-  updateProducts,
-} from '../controllers/productControllers.js';
-const router = express.Router();
+// import express from 'express';
+// import {
+//   deleteProducts,
+//   getAllProducts,
+//   postProducts,
+//   updateProducts,
+// } from '../controllers/productControllers.js';
+// const router = express.Router();
 
-router.route('/').get(getAllProducts).post(postProducts);
-router.route('/:id').patch(updateProducts).delete(deleteProducts);
+// router.route('/').get(getAllProducts).post(postProducts);
+// router.route('/:id').patch(updateProducts).delete(deleteProducts);
 
-export default router;
+// export default router;
